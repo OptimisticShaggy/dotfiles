bda06f4206749f28f82be139e6db82eade05d953
Vincent Maule <vincent.maule@gmail.com>
2023-11-08 20:05:45 +0000

    P2: Phone Region for Nextcloud.

--- hosts/estushlpse/nextcloud/nextcloud.nix
+++ hosts/estushlpse/nextcloud/nextcloud.nix
@@ -11,7 +11,7 @@
                 hostName = "estushlpse.narwhal-grue.ts.net";
                 database.createLocally = true;
                 datadir = "/mnt/runner/nextcloud";
-                defaultPhoneRegion = "United States";
+                defaultPhoneRegion = "US";
                 package = pkgs.nextcloud27;
                 https = true;
                 configureRedis = true;
