81fddd6bea0dce7c600cf3c3d994162c037067d4
Vincent Maule <vincent.maule@gmail.com>
2023-11-23 06:35:54 +0000

    Add DW to Config.

--- hosts/estushlpse/hardware-configuration.nix
+++ hosts/estushlpse/hardware-configuration.nix
@@ -89,6 +89,10 @@
         device = "/dev/disk/by-label/runner";
         fsType = "ext4";
       };
+      "/mnt/dw" = {
+        device = "/dev/disk/by-uuid/65577975-3b04-4fcc-86ee-bce0b9cc44af";
+        fsType = "ext4";
+      }
     };
 
   # Set your system kind (needed for flakes)
